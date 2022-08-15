0ccb9b0e96e91d556264d8c6c08b163c2459602f
jonatasmcampos <camposjonatas3@gmail.com>
2022-08-15 15:59:01 -0300

    navbar responsividade adicionada

--- src/components/navigationBar/Navbar.jsx
+++ src/components/navigationBar/Navbar.jsx
@@ -1,22 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Navbar.css"
 
 export default function Navbar() {
+    const [exibirBottomNavigation, setExibirBottomNavigation] = useState(false);
+    const [iconeMenu, setIconeMenu] = useState(true);
     return (
         <>
-            <nav className="center navigationbar">
-                <ul className="nav-items center">
+            {/* EXIBIÇÃO WEB */}
+            <nav className="center navigationbar no-mobile">
+                <ul className="nav-items center no-mobile">
                     <a href=""><li>Videoconferência&nbsp;</li></a>
                     <a href=""><li>| Ferramentas Google |</li></a>
                     <a href=""><li>&nbsp;Goconqr</li></a>
                 </ul>
-                <h1 className="titulo">Mantenha-se<br/>informado</h1>
-                <ul className="nav-items center">
+                <h1 className="titulo no-mobile">Mantenha-se<br />informado</h1>
+                <ul className="nav-items center no-mobile">
                     <a href=""><li>Trello&nbsp;</li></a>
                     <a href=""><li>| Google sala de aula |</li></a>
                     <a href=""><li> &nbsp;Escola digital</li></a>
                 </ul>
             </nav>
+
+            {/* BOTÃO DE MENU NO MOBILE */}
+            <div className="mobile bt-menu-mobile"
+                onClick={() => {
+                    if (exibirBottomNavigation) {
+                        setExibirBottomNavigation(false)
+                        setIconeMenu(true)
+                    } else {
+                        setExibirBottomNavigation(true)
+                        setIconeMenu(false)
+                    }
+                }}>
+                <h2 className="titulo mobile">Mantenha-se informado</h2>
+                <div className="bt-menu">
+                    {iconeMenu && <i class="fa fa-bars"></i>}
+                    {!iconeMenu && <i class="fa fa-times"></i>}
+                </div>
+            </div>
+
+            {/* EXIBIÇÃO MOBILE */}
+            <nav className={exibirBottomNavigation ? "mobile" : "no-mobile"}>
+                <ul className="nav-items center mobile">
+                    <a href=""><li>Videoconferência</li></a>
+                    <a href=""><li>Ferramentas Google</li></a>
+                    <a href=""><li>Goconqr</li></a>
+                    <a href=""><li>Trello</li></a>
+                    <a href=""><li>Google sala de aula</li></a>
+                    <a href=""><li>Escola digital</li></a>
+                </ul>
+            </nav>
+
         </>
     )
 }
