b444067473fe11353e764d593fdbf00ed1973e5b
jonatasmcampos <camposjonatas3@gmail.com>
2022-08-15 11:25:41 -0300

    header responsividade adicionada

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,32 @@
 import React from "react"
 import "./Header.css"
 import logo from "./../../images/logo2.jpg"
-import imgTitulo from "./../../images/img-titulo.png"
 
 export default function Header() {
     return (
         <>
             <header className="center">
-                <div className="div-esquerda">
+                {/* EXIBIÇÃO PARA WEB */}
+                <div className="div-esquerda no-mobile">
                     <div>
-                        <p>&nbsp;&nbsp;Simplificando seu trabalho.<br/>&nbsp;&nbsp;Aqui você encontrará conteúdos acerca das principais ferramentas digitais.</p>
-                        {/* <img src={imgTitulo} alt="Logo do site"/> */}
+                        <p>&nbsp;&nbsp;Simplificando seu trabalho.<br />&nbsp;&nbsp;Aqui você encontrará conteúdos acerca das principais ferramentas digitais.</p>
                     </div>
                 </div>
-                <div className="div-direita">
-                    <img src={logo} alt="Logo do site"/>
+                <div className="div-direita no-mobile">
+                    <img src={logo} alt="Logo do site" />
                 </div>
+
+                {/* EXIBIÇÃO PARA MOBILE */}
+                <div className="center mobile">
+                    <div className="div-imagem center mobile">
+                        <img src={logo} alt="Logo do site" />
+
+                        <div className="div-central center mobile">                          
+                            <p>Simplificando seu trabalho.<br />Disponibilizando conteúdos acerca das principais ferramentas digitais.</p>                           
+                        </div>
+                    </div>
+                </div>
+
             </header>
         </>
     )
