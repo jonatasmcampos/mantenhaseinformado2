95f47eea3dd7222dcfdfe5b4f06344d6a9b9fa24
jonatasmcampos <camposjonatas3@gmail.com>
2022-08-17 17:31:51 -0300

    Footer adicionado

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/navigationBar/Navbar"
 import Header from "./components/header/Header"
 import Principal from "./components/conteudoPrincipal/Principal"
+import Footer from "./components/footer/Footer"
 
 export default class Main extends Component {
     render() {
@@ -10,6 +11,7 @@ export default class Main extends Component {
                 <Navbar />
                 <Header />
                 <Principal />
+                <Footer />
             </>
         )
     }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,22 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <hr className='headerHR'/>
+            <div className='footer'>
+                <p>
+                    Site desenvolvido com intuito educacional,
+                    para praticar e aplicar conhecimentos do curso de Sistemas de Informação.
+                </p>
+                <p>
+                    Site desenvolvido para auxiliar os usuários das <br/> ferramentas digitais.
+                </p>
+                <p>
+                    Curso de graduação em Sistemas de Informação.<br />
+                    TCC2 <br/> 2022
+                </p>
+            </div>
+        </footer>
+    )
+}
